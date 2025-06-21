311351bf4e54f254401078b0681e659b47910375
cledson-leite <csbetsonline@gmail.com>
2025-06-21 13:15:37 -0300

    initial setup and create tabs navigaions

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,66 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { Tabs } from "expo-router";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { COLORS } from "@/constants/theme";
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+export default function TabsLayout() {
   return (
-    <Tabs
+    <Tabs 
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        // tabBarShowLabel: false,
         headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
+        tabBarActiveTintColor: COLORS.secondary,
+        tabBarInactiveTintColor: COLORS.grey,
+        tabBarStyle: {
+          backgroundColor: COLORS.background,
+          borderTopWidth: 0,
+          position: "absolute",
+          elevation: 0,
+          height: 60,
+          paddingBottom: 10
+        }
+      }}
+    >
+      <Tabs.Screen 
+      name="home"
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <FontAwesome name="home" size={size} color={color} />
+        ),
+      }}
       />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
+      <Tabs.Screen 
+      name="bookmarks"
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <FontAwesome name="bookmark" size={size} color={color} />
+        ),
+      }}
+      />
+      <Tabs.Screen 
+      name="create"
+      options={{
+        tabBarIcon: ({  size }) => (
+          <FontAwesome name="plus-circle" size={size} color={COLORS.primary} />
+        ),
+      }}
+      />
+      <Tabs.Screen 
+      name="notification"
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <FontAwesome name="heart" size={size} color={color} />
+        ),
+      }}
+      />
+      <Tabs.Screen 
+      name="profile"
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <FontAwesome name="user-circle" size={size} color={color} />
+        ),
+      }}
       />
     </Tabs>
-  );
+  )
 }
---
+++ app/(tabs)/home.tsx
@@ -0,0 +1,7 @@
+import { Link } from "expo-router";
+
+export default function Home() {
+  return (
+    <Link href="/notification">Vá para notificações</Link>
+  );
+}
