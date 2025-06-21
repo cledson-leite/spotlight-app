42a4525d1425325f4edd01360fe1d5a7f229079a
cledson-leite <csbetsonline@gmail.com>
2025-06-21 14:15:49 -0300

    prodected router and login screen wit oauth google and clerk

---
+++ app/(auth)/login.tsx
@@ -0,0 +1,55 @@
+import { COLORS } from '@/constants/theme'
+import { styles } from '@/styles/auth-style'
+import { useSSO } from '@clerk/clerk-expo'
+import FontAwesome from '@expo/vector-icons/FontAwesome'
+import { useRouter } from 'expo-router'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
+
+export default function Login() {
+  const {startSSOFlow} = useSSO()
+  const router = useRouter()
+
+  const handleGoogleSignIn = async () => {
+    try {
+      const {setActive, createdSessionId} = await startSSOFlow({strategy: "oauth_google"})
+      if(!createdSessionId || !setActive) return
+      setActive({session: createdSessionId})
+      router.replace("/(tabs)/home")
+    } catch (error) {
+      console.log('Erros ao logar com google ', error)
+    }
+  }
+  return (
+    <View style={styles.container}>
+      <View style={styles.brandSection}>
+        <View style={styles.logoContainer}>
+          <FontAwesome name="leaf" size={32} color={COLORS.primary} />
+        </View>
+        <Text style={styles.appName}>Spotlight</Text>
+        <Text style={styles.tagline}>Não perca nada!!!</Text>
+      </View>
+      <View style={styles.illustrationContainer}>
+        <Image 
+          source={require("../../assets/images/auth-bg-2.png")} 
+          style={styles.illustration}
+          resizeMode='cover'
+        />
+      </View>
+      <View style={styles.loginSection}>
+        <TouchableOpacity
+          style={styles.googleButton}
+          onPress={handleGoogleSignIn}
+          activeOpacity={0.9}
+        >
+          <View style={styles.googleIconContainer}>
+            <FontAwesome name="google" size={20} color={COLORS.surface} />
+          </View>
+          <Text style={styles.googleButtonText}>Entrar com Google</Text>
+        </TouchableOpacity>
+        <Text style={styles.termsText}>
+          Ao continuar, você concorda com nossos <Text style={{ color: COLORS.primary }}>Termos de uso</Text> e <Text style={{ color: COLORS.primary }}>Política de Privacidade</Text>
+          </Text>
+      </View>
+    </View>
+  )
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,13 +3,14 @@ import { Stack } from "expo-router";
 import { ClerkProvider } from '@clerk/clerk-expo'
 import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import InitialLayout from "./components/initial-layout";
 
 export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache}>
       <SafeAreaProvider>
         <SafeAreaView  style={{ flex: 1, backgroundColor: COLORS.background }}>
-            <Stack screenOptions={{ headerShown: false }}/>
+            <InitialLayout />
         </SafeAreaView>
       </SafeAreaProvider>
     </ClerkProvider>
